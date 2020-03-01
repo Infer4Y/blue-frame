b82078b7aba5c407d827424b199b885e19519f72
inferno4you <46350196+inferno4you@users.noreply.github.com>
2020-03-01 18:11:53 -0500

    Working on an updater for the future.

---
+++ blue_launcher/src/main/java/inferno/blue_launcher/Main.java
@@ -0,0 +1,6 @@
+package inferno.blue_launcher;
+
+public class Main {
+
+    public static void main(String[] args){}
+}
---
+++ src/main/java/inferno/blue_frame/common/Game.java
@@ -0,0 +1,5 @@
+package inferno.blue_frame.common;
+
+public class Game {
+    public static final String version = "0.0.0";
+}
