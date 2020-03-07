c8b7d67ef7b5533f6992cfcd2ab049397f5d7792
inferno4you <46350196+inferno4you@users.noreply.github.com>
2020-03-07 00:05:47 -0500

    Started the rework of the launcher.

---
+++ blue_update/src/main/java/inferno/blue_update/Updater.java
@@ -0,0 +1,37 @@
+package inferno.blue_update;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class Updater {
+    private final static String versionURL = "https://infer4y.github.io/blue-frame/updater/version.html";
+    private final static String historyURL = "https://infer4y.github.io/blue-frame/updater/history.html";
+    public static String getLatestVersion() throws Exception
+    {
+        String data = getData(versionURL);
+        return data.substring(data.indexOf("[version]")+9,data.indexOf("[/version]"));
+    }
+
+    public static String getWhatsNew() throws Exception
+    {
+        String data = getData(historyURL);
+        return data.substring(data.indexOf("[history]")+9,data.indexOf("[/history]"));
+    }
+
+    private static String getData(String address) throws IOException {
+        URL url = new URL(address);
+
+        InputStream html = url.openStream();
+
+        int c = 0;
+        StringBuilder buffer = new StringBuilder();
+
+        while(c != -1) {
+            c = html.read();
+
+            buffer.append((char)c);
+        }
+        return buffer.toString();
+    }
+}
